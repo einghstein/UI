a3cb9a17020797203c774eb9041f19e350f97cf6
einghstein <zdeninekf@seznam.cz>
2025-05-14 09:17:07 +0200

    initial commit for TableScreen.jsx

--- src/module/TableScreen.jsx
+++ src/module/TableScreen.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import { Container } from 'reactstrap';
 import { useTranslation } from 'react-i18next';
+import { TreeMenu } from 'asab_webui_components/dist/components/TreeMenu';
 
 export function TableScreen(props) {
 	const { t } = useTranslation();
+	const [data, setData] = React.useState([]);
+	const [loading, setLoading] = React.useState(true);
+	const [error, setError] = React.useState(null);
+
+	React.useEffect(() => {
+		fetch('https://devtest.teskalabs.com/data')
+			.then((res) => {
+				if (!res.ok) throw new Error('Network response was not ok');
+				return res.json();
+			})
+			.then((json) => {
+				setData(json);
+				setLoading(false);
+			})
+			.catch((err) => {
+				setError(err.message);
+				setLoading(false);
+			})
+	}, []);
+
+	if (loading) return <div>Loading...</div>;
+	if (error) return <div>Error: {error}</div>;
 
 	return (
 		<Container className='h-100'>
-			{t('Training|Hello, there is nothing here yet!')}
+			<TreeMenu data={data} />
 		</Container>
 	);
 }
