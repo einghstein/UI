3e41fac632d2c64b115b5ded87c276461f110e7a
einghstein <zdeninekf@seznam.cz>
2025-05-14 11:21:00 +0200

    made stuff display better

--- src/module/TableScreen.jsx
+++ src/module/TableScreen.jsx
@@ -1,39 +1,6 @@
 import React from 'react';
 import { Container } from 'reactstrap';
 
-function User({ id, name, email }) {
-	const [hovered, setHovered] = React.useState(false);
-
-	const handleMouseEnter = () => setHovered(true);
-	const handleMouseLeave = () => setHovered(false);
-
-	const liStyle = {
-		marginTop: "10px",
-		display: 'flex',
-		alignItems: 'center',
-		color: hovered ? '#000' : 'white',
-		backgroundColor: hovered ? '#e0e0e0' : 'transparent',
-		cursor: 'pointer',
-		padding: '10px',
-		borderRadius: '8px'
-	};
-
-	return (
-		<li
-			key={id}
-			style={liStyle}
-			onMouseEnter={handleMouseEnter}
-			onMouseLeave={handleMouseLeave}
-		>
-			<span style={{ minWidth: 160 }}>{name}</span>
-			<span style={{ marginLeft: 32 }}>{email}</span>
-			{hovered && (
-				<span style={{ marginLeft: 'auto', color: '#888' }}>ID: {id}</span>
-			)}
-		</li>
-	);
-}
-
 
 export function TableScreen(props) {
 	const [data, setData] = React.useState([]);
@@ -63,11 +30,14 @@ export function TableScreen(props) {
 	console.log('data', data);
 	return (
 		<Container className='h-100'>
-			<dataTable2 className='h-100'>
+			<table className='h-100'>
 				<thead>
 					<tr>
 						<th>Name</th>
 						<th>Email</th>
+						<th>Address</th>
+						<th>Last Sign In</th>
+						<th>Created</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -75,22 +45,22 @@ export function TableScreen(props) {
 						<tr key={userData.id}>
 							<td>{userData.name || userData.fullName || userData.username}</td>
 							<td>{userData.email}</td>
+							<td>{userData.address}</td>
+							<td>
+								{userData.last_sign_in
+									? new Date(userData.last_sign_in * 1000).toLocaleString()
+									: ''}
+							</td>
+							<td>
+								{userData.created
+									? new Date(userData.created * 1000).toLocaleString()
+									: ''}
+							</td>
 						</tr>
 					))}
 				</tbody>
-			</dataTable2>
-			<ul>
-				{data.map((userData) => (
-					<User
-						key={userData.id}
-						id={userData.id}
-						name={userData.name || userData.fullName || userData.username}
-						email={userData.email}
-					/>
-				))}
-			</ul>
+			</table>
 		</Container>
-		
 	);
 	
 }
