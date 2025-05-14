3ebaba8a0dd4e0a91ab62a519c53c69f6b7d483b
einghstein <zdeninekf@seznam.cz>
2025-05-14 11:01:48 +0200

    Last time seeing User function

--- src/module/TableScreen.jsx
+++ src/module/TableScreen.jsx
@@ -2,10 +2,34 @@ import React from 'react';
 import { Container } from 'reactstrap';
 
 function User({ id, name, email }) {
-	console.log('User', id, name, email);
+	const [hovered, setHovered] = React.useState(false);
+
+	const handleMouseEnter = () => setHovered(true);
+	const handleMouseLeave = () => setHovered(false);
+
+	const liStyle = {
+		marginTop: "10px",
+		display: 'flex',
+		alignItems: 'center',
+		color: hovered ? '#000' : 'white',
+		backgroundColor: hovered ? '#e0e0e0' : 'transparent',
+		cursor: 'pointer',
+		padding: '10px',
+		borderRadius: '8px'
+	};
+
 	return (
-		<li key={id}>
-			{name}    {email}
+		<li
+			key={id}
+			style={liStyle}
+			onMouseEnter={handleMouseEnter}
+			onMouseLeave={handleMouseLeave}
+		>
+			<span style={{ minWidth: 160 }}>{name}</span>
+			<span style={{ marginLeft: 32 }}>{email}</span>
+			{hovered && (
+				<span style={{ marginLeft: 'auto', color: '#888' }}>ID: {id}</span>
+			)}
 		</li>
 	);
 }
@@ -39,12 +63,31 @@ export function TableScreen(props) {
 	console.log('data', data);
 	return (
 		<Container className='h-100'>
+			<dataTable2 className='h-100'>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Email</th>
+					</tr>
+				</thead>
+				<tbody>
+					{data.map((userData) => (
+						<tr key={userData.id}>
+							<td>{userData.name || userData.fullName || userData.username}</td>
+							<td>{userData.email}</td>
+						</tr>
+					))}
+				</tbody>
+			</dataTable2>
 			<ul>
-				{data.map((userData, idx) => {
-					return (
-						User(userData)
-					)
-				})}
+				{data.map((userData) => (
+					<User
+						key={userData.id}
+						id={userData.id}
+						name={userData.name || userData.fullName || userData.username}
+						email={userData.email}
+					/>
+				))}
 			</ul>
 		</Container>
 		
